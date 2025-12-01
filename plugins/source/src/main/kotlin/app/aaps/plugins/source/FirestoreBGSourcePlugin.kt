eb782f72502fe78eea32d5ab5d9413bc94569801
lizishtaif <lizishtaifcgm@gmail.com>
2025-12-01 17:21:11 +0200

    wip

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/FirestoreBGSourcePlugin.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/FirestoreBGSourcePlugin.kt
@@ -1,29 +1,35 @@
 package app.aaps.plugins.source
 
-import app.aaps.core.interfaces.configuration.Config
+import app.aaps.core.data.model.GV
+import app.aaps.core.data.model.SourceSensor
+import app.aaps.core.data.model.TrendArrow
+import app.aaps.core.data.plugin.PluginType
+import app.aaps.core.data.ue.Sources
+import app.aaps.core.interfaces.db.PersistenceLayer
 import app.aaps.core.interfaces.logging.AAPSLogger
 import app.aaps.core.interfaces.logging.LTag
-import app.aaps.core.interfaces.plugin.PluginBase
 import app.aaps.core.interfaces.plugin.PluginDescription
-import app.aaps.core.interfaces.plugin.PluginType
 import app.aaps.core.interfaces.resources.ResourceHelper
 import app.aaps.core.interfaces.source.BgSource
-import app.aaps.core.interfaces.utils.T
-import app.aaps.database.entities.GlucoseValue
-import app.aaps.database.impl.AppRepository
-import app.aaps.database.impl.transactions.CgmSourceTransaction
-import app.aaps.database.transactions.TransactionGlucoseValue
+import app.aaps.core.objects.workflow.LoggingWorker
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 import dagger.android.HasAndroidInjector
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.kotlin.plusAssign
 import javax.inject.Inject
 import javax.inject.Singleton
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.DocumentChange
-import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.QueryDocumentSnapshot
 import java.util.Calendar
+import kotlinx.coroutines.Dispatchers
+import app.aaps.core.interfaces.configuration.Config
+import app.aaps.core.interfaces.plugin.PluginBase
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resumeWithException
 
 @Singleton
 class FirestorePlugin @Inject constructor(
@@ -34,7 +40,7 @@ class FirestorePlugin @Inject constructor(
     PluginDescription()
         .mainType(PluginType.BGSOURCE)
         .fragmentClass(BGSourceFragment::class.java.name)
-        .pluginIcon(app.aaps.core.main.R.drawable.ic_firebase_bg)
+        .pluginIcon(app.aaps.core.objects.R.drawable.ic_firebase_bg)
         .preferencesId(R.xml.pref_firebase)
         .pluginName(R.string.firestore_bg)
         .shortName(R.string.firestore_bg_short)
@@ -51,67 +57,70 @@ class FirestorePlugin @Inject constructor(
 
         @Inject lateinit var injector: HasAndroidInjector
         @Inject lateinit var firebasePlugin: FirestorePlugin
+        @Inject lateinit var persistenceLayer: PersistenceLayer
 
-        private var listener: ListenerRegistration? = null
         private val disposable = CompositeDisposable()
 
-        override fun advancedFilteringSupported(): Boolean = true
-
-        override fun onStart() {
-            super.onStart()
-            aapsLogger.info(LTag.FIRESTORE, "starting listener")
-            if (listener != null) {
-                listener?.remove()
-            }
-
-            val cal = Calendar.getInstance()
-            cal.add(Calendar.MINUTE, -5)
-            val startDateMs = cal.timeInMillis
-
-            listener = Firebase.firestore.collection("entries").whereGreaterThan("date", startDateMs).addSnapshotListener { snapshots, e ->
-                    if (e != null) {
-                        aapsLogger.error(LTag.FIRESTORE, "error listening to firebase", e)
-                        return@addSnapshotListener
-                    }
-
-                    for (dc in snapshots!!.documentChanges) {
-                        val type = dc.type
-                        aapsLogger.info(LTag.FIRESTORE, "got new snapshot event $type  ${dc.document.data}")
-                        if (type == DocumentChange.Type.ADDED) {
-                            doWorkAndLog(dc.document)
-
-                        }
-                    }
-                }
-        }
-
-        override fun onStop() {
-            super.onStop()
-            listener?.remove()
-            listener = null
-            disposable.clear()
-        }
-
         @SuppressLint("CheckResult")
-        override suspend fun doWorkAndLog(snapshot: QueryDocumentSnapshot): Result {
-            var ret = Result.success()
-            val inputData = snapshot.data
+        private fun doWorkAndLog(document: QueryDocumentSnapshot) {
+            if (!firebasePlugin.isEnabled()) return
 
-            if (!firebasePlugin.isEnabled()) return Result.success(workDataOf("Result" to "Plugin not enabled"))
-            aapsLogger.debug(LTag.BGSOURCE, "Received Glimp Data: $inputData}")
+            val data = document.data
+            aapsLogger.debug(LTag.BGSOURCE, "Received Firestore Data: $data")
             val glucoseValues = mutableListOf<GV>()
             glucoseValues += GV(
-                timestamp = inputData["date"].toString().toLong(),
-                value = inputData["sgv"].toString().toDouble(),
+                timestamp = data["date"].toString().toLong(),
+                value = data["sgv"].toString().toDouble(),
                 raw = 0.0,
                 noise = null,
-                trendArrow = TrendArrow.fromString(inputData["direction"].toString()),
-                sourceSensor = GlucoseValue.SourceSensor.fromString(inputData["device"].toString())
+                trendArrow = TrendArrow.fromString(data["direction"].toString()),
+                sourceSensor = SourceSensor.fromString(data["device"].toString())
             )
             persistenceLayer.insertCgmSourceData(Sources.Firebase, glucoseValues, emptyList(), null)
-                .doOnError { ret = Result.failure(workDataOf("Error" to it.toString())) }
-                .blockingGet()
-            return ret
+                .doOnError { aapsLogger.error(LTag.FIRESTORE, "Error inserting CGM data", it) }
+                .subscribe()
+                .let { disposable.add(it) }
+        }
+
+        override suspend fun doWorkAndLog(): Result {
+            if (!firebasePlugin.isEnabled()) return Result.success(workDataOf("Result" to "Plugin not enabled"))
+
+            aapsLogger.info(LTag.FIRESTORE, "starting listener")
+
+            suspendCancellableCoroutine<Unit> { continuation ->
+                val cal = Calendar.getInstance()
+                cal.add(Calendar.MINUTE, -5)
+                val startDateMs = cal.timeInMillis
+
+                val listener = Firebase.firestore.collection("entries")
+                    .whereGreaterThan("date", startDateMs)
+                    .addSnapshotListener { snapshots, e ->
+                        if (e != null) {
+                            aapsLogger.error(LTag.FIRESTORE, "error listening to firebase", e)
+                            if (continuation.isActive) {
+                                continuation.resumeWithException(e)
+                            }
+                            return@addSnapshotListener
+                        }
+
+                        for (dc in snapshots!!.documentChanges) {
+                            if (dc.type == DocumentChange.Type.ADDED) {
+                                aapsLogger.info(
+                                    LTag.FIRESTORE,
+                                    "got new snapshot event ${dc.type}  ${dc.document.data}"
+                                )
+                                doWorkAndLog(dc.document)
+                            }
+                        }
+                    }
+
+                continuation.invokeOnCancellation {
+                    aapsLogger.info(LTag.FIRESTORE, "stopping listener")
+                    listener.remove()
+                    disposable.clear()
+                }
+            }
+            return Result.success()
         }
     }
 }
